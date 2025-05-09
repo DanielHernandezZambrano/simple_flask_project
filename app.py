e178e36d4ccc3e2e154311af11aa232c18cec3d4
Daniel <xdanielyalejandro@hotmail.com>
2025-05-09 10:33:30 -0300

    Añadiendo flask-bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app) # Bootstrap se inicializa a la app de Flask
 
 # Lista de items
 items = ["Palta", "Banana", "Tomate", "Cebolla", "Pimiento Rojo", "Maple de huevos"]
