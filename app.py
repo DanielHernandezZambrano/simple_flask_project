7a9854d5a198b462c98d86fc4e97bfcffed17457
Daniel <xdanielyalejandro@hotmail.com>
2025-05-09 11:07:41 -0300

    Añadiendo session y encriptación de cookie

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
 bootstrap = Bootstrap(app) # Bootstrap se inicializa a la app de Flask
+app.config['SECRET_KEY'] = "CLAVE SEGURA"  # Cambio la configuración de la key secreta
 
 # Lista de items
 items = ["Palta", "Banana", "Tomate", "Cebolla", "Pimiento Rojo", "Maple de huevos"]
@@ -15,13 +15,15 @@ def index():
 
     response = make_response(redirect("/show_information_address"))  # Crea una respuesta y la redirecciona a otra url
 
-    response.set_cookie("user_ip_information", user_ip_information) # Crea una cookie
+    #response.set_cookie("user_ip_information", user_ip_information) # Crea una cookie
+    session['user_ip_information'] = user_ip_information
     return response
     #return f"Tu direccion IP es: {user_ip_info}"
 
 @app.route("/show_information_address")
 def show_information():
-    user_ip = request.cookies.get("user_ip_information")
+    #user_ip = request.cookies.get("user_ip_information")
+    user_ip = session.get("user_ip_information")
 
     context = {
         "user_ip": user_ip,
